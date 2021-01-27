cd351d72469587b55d394b67f30f71c857c54efd
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-01-27 17:57:23 -0300

    feat(web): CarsHead component

---
+++ web/src/components/CarsHead/CarsHead.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Container, Grid } from "@material-ui/core";
+
+import CarsFilter from "../CarsFilter/CarsFilter";
+
+import "./CarsHead.styles.scss";
+
+const CarsHead = () => {
+  return (
+    <section title="Cars head" className="cars-head">
+      <Container>
+        <Grid container spacing={2} justify="center">
+          <Grid item container xs={12} justify="center">
+            <h1 className="cars-head-title">Cars</h1>
+          </Grid>
+          <Grid item container xs={12}>
+            <CarsFilter />
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  );
+};
+
+export default CarsHead;
