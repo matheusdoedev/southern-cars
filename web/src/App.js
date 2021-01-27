ef0ec7e029f22abb00217ce4d3cd921f0dbd74d1
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-01-27 17:54:38 -0300

    chore(web): contexts imported
    Modal and Cars contexts added

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-import Routes from "./pages/Index";
+import Routes from "./routes";
+import { ModalContextComponent } from "./hooks/modalContext";
+import { CarsContextComponent } from "./hooks/carsContext";
 
 import "./App.scss";
 
 const App = () => {
   return (
-    <>
-      <Routes />
-    </>
+    <ModalContextComponent>
+      <CarsContextComponent>
+        <Routes />
+      </CarsContextComponent>
+    </ModalContextComponent>
   );
 };
 
