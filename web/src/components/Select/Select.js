15f4320859bce02e22da1a8d567d8ab7fe06ca5a
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-01-27 17:59:36 -0300

    feat(web): Select component

---
+++ web/src/components/Select/Select.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import SelectInput from "@material-ui/core/Select";
+
+const Select = ({
+  name,
+  value,
+  setValue,
+  defaultValue,
+  label,
+  options,
+  ...props
+}) => {
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <FormControl variant="filled">
+      <InputLabel id={name}>{label}</InputLabel>
+      <SelectInput
+        labelId={defaultValue}
+        id={name}
+        value={value}
+        onChange={handleChange}
+        {...props}
+      >
+        {options?.map((option) => (
+          <MenuItem value={option.value} key={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </SelectInput>
+    </FormControl>
+  );
+};
+
+export default Select;
