ce1056e73c8644111fe29b083b370a8ebbe9db33
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-03-09 16:59:25 -0300

    :construction: feat: cars-filters component

---
+++ web/src/app/components/cars-filters/cars-filters.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cars-filters',
+  templateUrl: './cars-filters.component.html',
+  styleUrls: ['./cars-filters.component.scss'],
+})
+export class CarsFiltersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
