a22616c65a54793d232ca8265473c1ba2828bf85
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2021-03-09 16:59:46 -0300

    :construction: feat: cars-results component

---
+++ web/src/app/components/cars-results/cars-results.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cars-results',
+  templateUrl: './cars-results.component.html',
+  styleUrls: ['./cars-results.component.scss'],
+})
+export class CarsResultsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
