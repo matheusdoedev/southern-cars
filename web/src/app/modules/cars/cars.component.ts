f1ef6a7d8f871f2e3a83cf71a54ca4d6770ca3df
matheusdoedev <matheusdoe.dev@gmail.com>
2021-03-12 11:27:42 -0300

    :construction: chore: car service added as provider in cars component

--- web/src/app/modules/cars/cars.component.ts
+++ web/src/app/modules/cars/cars.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'cars-root',
   templateUrl: './cars.component.html',
+  providers: [CarService],
 })
 export class CarsComponent {}
