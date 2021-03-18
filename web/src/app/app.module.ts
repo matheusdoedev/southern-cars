26395ecd659f6a0ca080e210b899881e4ff56400
matheusdoedev <matheusdoe.dev@gmail.com>
2021-03-18 17:52:11 -0300

    :construction: chore: SharedModule imported in app.module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from './shared/shared.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
